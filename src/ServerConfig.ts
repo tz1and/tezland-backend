de35f3f321b8ae3dfb8a1bc1b202ca3e711242bc
852Kerfunkle <852Kerfunkle@users.noreply.github.com>
2022-01-23 15:55:08 +0100

    fixes and stuff

--- src/ServerConfig.ts
+++ src/ServerConfig.ts
@@ -1,5 +1,5 @@
 import { config as dotenvFlowConfig } from 'dotenv-flow'
-dotenvFlowConfig();
+dotenvFlowConfig({ silent: process.env.NODE_ENV !== "development" });
 
 class ServerConfig {
     public NFTSTORAGE_API_KEY: string;
--- src/storage/localipfs.ts
+++ src/storage/localipfs.ts
@@ -1,11 +1,16 @@
+import assert from 'assert';
 import * as ipfs from 'ipfs-http-client';
 import { Blob } from 'nft.storage';
 import { performance } from 'perf_hooks';
 import ServerConfig from '../ServerConfig';
 
-const ipfs_client = ipfs.create({ url: ServerConfig.LOCAL_IPFS_URL });
+const ipfs_client = ServerConfig.USE_LOCAL_IPFS ? ipfs.create({ url: ServerConfig.LOCAL_IPFS_URL }) : null;
 
 export const uploadToLocal = async (data: any): Promise<any> => {
+    if(!ipfs_client) {
+        assert(false, "uploadToLocal called but ipfs client is null");
+    }
+
     const start_time = performance.now()
     // first we upload every blob in the object
     for (var property in data) {

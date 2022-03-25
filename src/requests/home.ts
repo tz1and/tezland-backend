7856624ca2e8dbf1083fff487b72f57bc554de6b
852Kerfunkle <852Kerfunkle@users.noreply.github.com>
2022-03-25 14:07:57 +0100

    default route has a cute kaomoji now

--- src/requests/home.ts
+++ src/requests/home.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express'
 
 export const defaultRoute = async (req: Request, res: Response) => {
-    res.send( "Nothing here!" );
+    res.send( "There's nothing here! &star;&profline;&lpar;&gt;&#x3002;&lt;&rpar;" );
 }

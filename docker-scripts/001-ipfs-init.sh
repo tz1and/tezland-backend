4d6f4818ef2ee62e916a3f30b6660af2728a45a0
852Kerfunkle <852Kerfunkle@users.noreply.github.com>
2022-06-21 01:03:57 +0200

    backend ipfs config

--- docker-scripts/001-ipfs-init.sh
+++ docker-scripts/001-ipfs-init.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 set -ex
 ipfs config --json API.HTTPHeaders.Access-Control-Allow-Origin '["*"]'
+ipfs config --json Datastore.StorageMax '"30GB"'
+ipfs config --json Datastore.GCPeriod '"1h"'

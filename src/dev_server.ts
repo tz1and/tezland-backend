70c4b075ff7fb734a2e5500e6347b9d90f13109f
852Kerfunkle <852Kerfunkle@users.noreply.github.com>
2022-06-22 23:33:39 +0200

    style

--- src/dev_server.ts
+++ src/dev_server.ts
@@ -1,7 +1,7 @@
-import { config as dotenvFlowConfig } from 'dotenv-flow'
+import { config as dotenvFlowConfig } from 'dotenv-flow';
 import { isDev } from './utils/Utils';
 dotenvFlowConfig({ silent: !isDev() });
 
-import './express_worker'
-import './websocket_worker'
-import './gateway_worker'
+import './express_worker';
+import './websocket_worker';
+import './gateway_worker';

1be20b086034af0004b4f15e1318c9a7087b0c33
852Kerfunkle <852Kerfunkle@users.noreply.github.com>
2022-10-28 16:47:16 +0200

    ipfs docker fixes and stuff

--- docker-scripts/001-ipfs-init.sh
+++ docker-scripts/001-ipfs-init.sh
@@ -3,3 +3,6 @@ set -ex
 ipfs config --json API.HTTPHeaders.Access-Control-Allow-Origin '["*"]'
 ipfs config --json Datastore.StorageMax '"30GB"'
 ipfs config --json Datastore.GCPeriod '"1h"'
+ipfs config --json Addresses.API '"/ip4/0.0.0.0/tcp/5001"'
+ipfs config --json Addresses.Gateway '"/ip4/0.0.0.0/tcp/8080"'
+ipfs config --json Addresses.Swarm '[ "/ip4/0.0.0.0/tcp/4001", "/ip6/::/tcp/4001", "/ip4/0.0.0.0/udp/4001/quic", "/ip6/::/udp/4001/quic" ]'
